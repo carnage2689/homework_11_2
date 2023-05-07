87534a36885b92c4b857434b64d3f89836837d4c
vadim <carnage2689@gmail.com>
2023-05-08 00:13:43 +0300

    update str_func

--- str_func.py
+++ str_func.py
@@ -1,3 +1,9 @@
 def upper_func():
+    """возвращает текст со всеми заглавными буквами"""
     text = input().upper()
     print(text)
+
+def title_func():
+    """возвращает текст с первыми заглавными буквами"""
+    text_2 = input().title()
+    print(text_2)
